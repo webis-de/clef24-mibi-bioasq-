6e1690a0c0b2c82ec3c14cee45c1252bbd2f207e
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-03-07 23:00:53 +0100

    Fix incremental answer module

--- mibi/modules/incremental.py
+++ mibi/modules/incremental.py
@@ -17,23 +17,23 @@ class PredictNextTask(Signature):
     """
     Chose the next task to run, in order to get the full answer.
     """
-    has_documents: bool = InputField(
+    has_documents: str = InputField(
         prefix="Has retrieved documents:",
         desc="If documents were already retrieved for the question."
     )
-    has_snippets: bool = InputField(
+    has_snippets: str = InputField(
         prefix="Has retrieved snippets:",
         desc="If snippets were already retrieved for the question."
     )
-    has_exact_answer: bool = InputField(
+    has_exact_answer: str = InputField(
         prefix="Has generated exact answer:",
         desc="If an exact answer was already generated for the question."
     )
-    has_ideal_answer: bool = InputField(
+    has_ideal_answer: str = InputField(
         prefix="Has generated summary answer:",
         desc="If a human summary answer was already generated for the question."
     )
-    task: Task = OutputField(
+    task: str = OutputField(
         desc=(
             f"Must always be one of '{Task.make_documents}', "
             f"'{Task.make_snippets}', '{Task.make_exact_answer}', or "
@@ -43,13 +43,14 @@ class PredictNextTask(Signature):
     )
 
 
-class IncrementalAnswerModule(Module, AnswerModule):
+# class IncrementalAnswerModule(AnswerModule, Module):
+class IncrementalAnswerModule(Module):
     """
     Incrementally build the full answer by asking the LLM which
     part of the answer (i.e., documents, snippets, exact answer,
     or ideal answer) to make next.
     """
-    
+
     _documents_module: DocumentsModule
     _snippets_module: SnippetsModule
     _exact_answer_module: ExactAnswerModule
@@ -78,19 +79,35 @@ class IncrementalAnswerModule(Module, AnswerModule):
         )
         while not builder.is_ready:
             current_answer = builder.partial_answer
-            next_task = self._next_task(
-                has_documents=current_answer.documents is not None,
-                has_snippets=current_answer.snippets is not None,
-                has_exact_answer=current_answer.exact_answer is not None,
-                has_ideal_answer=current_answer.ideal_answer is not None,
+            next_task: str = self._next_task.forward(
+                has_documents=(
+                    "yes"
+                    if current_answer.documents is not None
+                    else "no"
+                ),
+                has_snippets=(
+                    "yes"
+                    if current_answer.snippets is not None
+                    else "no"
+                ),
+                has_exact_answer=(
+                    "yes"
+                    if current_answer.exact_answer is not None
+                    else "no"
+                ),
+                has_ideal_answer=(
+                    "yes"
+                    if current_answer.ideal_answer is not None
+                    else "no"
+                ),
             ).answer
-            if next_task == Task.make_documents:
+            if next_task == Task.make_documents.value:
                 builder.make_documents()
-            if next_task == Task.make_snippets:
+            if next_task == Task.make_snippets.value:
                 builder.make_snippets()
-            if next_task == Task.make_exact_answer:
+            if next_task == Task.make_exact_answer.value:
                 builder.make_exact_answer()
-            if next_task == Task.make_ideal_answer:
+            if next_task == Task.make_ideal_answer.value:
                 builder.make_ideal_answer()
             else:
                 warn(RuntimeWarning(
