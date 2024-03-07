7754ac52bd1514c81820de6141e8116de3fa010b
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-03-07 20:40:47 +0100

    Improve base modules

--- mibi/modules/__init__.py
+++ mibi/modules/__init__.py
@@ -1,5 +1,5 @@
 from typing import Protocol
-from mibi.model import Question, PartialAnswer, Documents, Snippets, ExactAnswer, IdealAnswer
+from mibi.model import Question, PartialAnswer, Documents, Snippets, ExactAnswer, IdealAnswer, YesNoExactAnswer, FactoidExactAnswer, ListExactAnswer, SummaryExactAnswer, NOT_AVAILABLE
 
 
 class DocumentsMaker(Protocol):
@@ -10,6 +10,13 @@ class DocumentsMaker(Protocol):
     ) -> Documents:
         raise NotImplementedError()
 
+    def __call__(
+        self,
+        question: Question,
+        partial_answer: PartialAnswer,
+    ) -> Documents:
+        return self.make_documents(question, partial_answer)
+
 
 class SnippetsMaker(Protocol):
     def make_snippets(
@@ -18,6 +25,13 @@ class SnippetsMaker(Protocol):
         partial_answer: PartialAnswer,
     ) -> Snippets:
         raise NotImplementedError()
+
+    def __call__(
+        self,
+        question: Question,
+        partial_answer: PartialAnswer,
+    ) -> Snippets:
+        return self.make_snippets(question, partial_answer)
 
 
 class ExactAnswerMaker(Protocol):
@@ -28,6 +42,59 @@ class ExactAnswerMaker(Protocol):
     ) -> ExactAnswer:
         raise NotImplementedError()
 
+    def __call__(
+        self,
+        question: Question,
+        partial_answer: PartialAnswer,
+    ) -> ExactAnswer:
+        return self.make_exact_answer(question, partial_answer)
+
+
+class SwitchExactAnswerMaker(ExactAnswerMaker, Protocol):
+    def make_exact_answer(
+        self,
+        question: Question,
+        partial_answer: PartialAnswer,
+    ) -> ExactAnswer:
+        if question.type == "yesno":
+            return self.make_yes_no_exact_answer(question, partial_answer)
+        elif question.type == "factoid":
+            return self.make_factoid_exact_answer(question, partial_answer)
+        elif question.type == "list":
+            return self.make_list_exact_answer(question, partial_answer)
+        elif question.type == "summary":
+            return self.make_summary_exact_answer(question, partial_answer)
+        else:
+            raise ValueError(f"Unknown question type: {question.type}")
+
+    def make_yes_no_exact_answer(
+        self,
+        question: Question,
+        partial_answer: PartialAnswer,
+    ) -> YesNoExactAnswer:
+        raise NotImplementedError()
+
+    def make_factoid_exact_answer(
+        self,
+        question: Question,
+        partial_answer: PartialAnswer,
+    ) -> FactoidExactAnswer:
+        raise NotImplementedError()
+
+    def make_list_exact_answer(
+        self,
+        question: Question,
+        partial_answer: PartialAnswer,
+    ) -> ListExactAnswer:
+        raise NotImplementedError()
+
+    def make_summary_exact_answer(
+        self,
+        question: Question,
+        partial_answer: PartialAnswer,
+    ) -> SummaryExactAnswer:
+        return NOT_AVAILABLE
+
 
 class IdealAnswerMaker(Protocol):
     def make_ideal_answer(
@@ -36,3 +103,10 @@ class IdealAnswerMaker(Protocol):
         partial_answer: PartialAnswer,
     ) -> IdealAnswer:
         raise NotImplementedError()
+
+    def __call__(
+        self,
+        question: Question,
+        partial_answer: PartialAnswer,
+    ) -> IdealAnswer:
+        return self.make_ideal_answer(question, partial_answer)
