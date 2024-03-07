8bc4e04aae8153aabbd76f2a51e327c6a0ac3c7a
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-03-07 20:39:49 +0100

    Simplify model

--- mibi/model.py
+++ mibi/model.py
@@ -1,16 +1,15 @@
-from typing import Annotated, Generic, Literal, TypeVar, TypeAlias
-from annotated_types import Len
-from pydantic import HttpUrl, BaseModel, Field
+from typing import Annotated, Final, Literal, TypeAlias, Sequence
+from annotated_types import Len, Ge
+from pydantic import AliasChoices, BaseModel, Field, UrlConstraints
+from pydantic_core import Url
 
 
-QuestionType = Literal[
+QuestionType: TypeAlias = Literal[
     "yesno",
     "factoid",
     "list",
     "summary",
 ]
-
-Q = TypeVar("Q", bound=QuestionType)
 
 QuestionId = Annotated[
     str,
@@ -18,35 +17,66 @@ QuestionId = Annotated[
 ]
 
 
-class Question(BaseModel, Generic[Q]):
+class Question(BaseModel):
     id: QuestionId
-    type: Q
+    type: QuestionType
     body: str
 
 
-Document: TypeAlias = HttpUrl
+PubMedUrl = Annotated[
+    Url,
+    UrlConstraints(
+        max_length=2083,
+        allowed_schemes=['http', 'https'],
+        host_required=True,
+    ),
+]
 
-Documents: TypeAlias = list[Document]
+Document: TypeAlias = PubMedUrl
+
+Documents: TypeAlias = Annotated[
+    list[Document],
+    Len(min_length=1),
+]
 
 
 class Snippet(BaseModel):
-    document: Document
-    text: str
-    offset_in_begin_section: int = Field(
-        alias="offsetInBeginSection",
+    document: Document = Field(frozen=True)
+    text: str = Field(frozen=True)
+    offset_in_begin_section: Annotated[
+        int,
+        Ge(0),
+    ] = Field(
+        frozen=True,
+        validation_alias=AliasChoices(
+            "offset_in_begin_section", "offsetInBeginSection"),
+        serialization_alias="offsetInBeginSection",
     )
-    offset_in_end_section: int = Field(
-        alias="offsetInEndSection",
+    offset_in_end_section: Annotated[
+        int,
+        Ge(0),
+    ] = Field(
+        frozen=True,
+        validation_alias=AliasChoices(
+            "offset_in_end_section", "offsetInEndSection"),
+        serialization_alias="offsetInEndSection",
     )
     begin_section: str = Field(
-        alias="beginSection",
+        frozen=True,
+        validation_alias=AliasChoices("begin_section", "beginSection"),
+        serialization_alias="beginSection",
     )
     end_section: str = Field(
-        alias="endSection",
+        frozen=True,
+        validation_alias=AliasChoices("end_section", "endSection"),
+        serialization_alias="endSection",
     )
 
 
-Snippets: TypeAlias = list[Snippet]
+Snippets: TypeAlias = Annotated[
+    list[Snippet],
+    Len(min_length=1),
+]
 
 YesNoExactAnswer: TypeAlias = Literal["yes", "no"]
 
@@ -56,7 +86,7 @@ FactoidExactAnswer: TypeAlias = Annotated[
 ]
 
 ListExactAnswer: TypeAlias = Annotated[
-    list[
+    Sequence[
         Annotated[
             list[str],
             Len(min_length=1),
@@ -65,7 +95,9 @@ ListExactAnswer: TypeAlias = Annotated[
     Len(min_length=1),
 ]
 
-SummaryExactAnswer: TypeAlias = None
+
+SummaryExactAnswer: TypeAlias = Literal["n/a"]
+NOT_AVAILABLE: Final[SummaryExactAnswer] = "n/a"
 
 IdealAnswer: TypeAlias = Annotated[
     list[str],
@@ -76,17 +108,17 @@ ExactAnswer: TypeAlias = YesNoExactAnswer | FactoidExactAnswer | ListExactAnswer
 
 
 class PartialAnswer(BaseModel):
-    documents: Documents | None = None
-    snippets: Snippets | None = None
-    ideal_answer: IdealAnswer | None = None
-    exact_answer: ExactAnswer | None = None
+    documents: Documents | None = Field(frozen=True, default=None)
+    snippets: Snippets | None = Field(frozen=True, default=None)
+    ideal_answer: IdealAnswer | None = Field(frozen=True, default=None)
+    exact_answer: ExactAnswer | None = Field(frozen=True, default=None)
 
 
 class Answer(BaseModel):
-    documents: Documents
-    snippets: Snippets
-    ideal_answer: IdealAnswer
-    exact_answer: ExactAnswer
+    documents: Documents = Field(frozen=True)
+    snippets: Snippets = Field(frozen=True)
+    ideal_answer: IdealAnswer = Field(frozen=True)
+    exact_answer: ExactAnswer = Field(frozen=True)
 
 
 class TrainingQuestion(Question, PartialAnswer):
@@ -94,4 +126,4 @@ class TrainingQuestion(Question, PartialAnswer):
 
 
 class TrainingData(BaseModel):
-    questions: list[TrainingQuestion]
+    questions: list[TrainingQuestion] = Field(frozen=True)
