a0cbf2a6fdf5a3ef19eb70ac8810a838987fa892
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-03-14 12:44:31 +0100

    Improve incremental answer module with typed prediction

--- mibi/modules/incremental.py
+++ mibi/modules/incremental.py
@@ -1,4 +1,4 @@
-from typing import Callable, Literal, ParamSpec, TypeAlias, TypeVar, cast
+from typing import Literal, Sequence, TypeAlias, cast
 from warnings import warn
 from dspy import Module, Signature, Prediction, InputField, OutputField, TypedPredictor
 from pydantic import BaseModel, Field
@@ -7,6 +7,7 @@ from pyrate_limiter import Rate, Duration
 from mibi.builder import AnswerBuilder
 from mibi.model import Question, Answer, QuestionType
 from mibi.modules import DocumentsModule, SnippetsModule, ExactAnswerModule, IdealAnswerModule
+from mibi.utils.rate_limiting import rate_limit
 
 
 Task: TypeAlias = Literal[
@@ -24,50 +25,31 @@ YesNo: TypeAlias = Literal[
 ]
 
 
-class PredictNextTaskInput(BaseModel):
+class HistoryItem(BaseModel):
+    task: Task = Field(description="The task that was run.")
+    successful: bool = Field(
+        description="Whether the task was run successful.")
+
+
+class NextTaskInput(BaseModel):
     question: str = Field(description="The question that should be answered.")
     question_type: QuestionType = Field(
         description="How the question should be answered.")
-    has_documents: bool = Field(
-        description="If documents were already retrieved for the question.")
-    has_snippets: bool = Field(
-        description="If snippets were already retrieved for the question.")
-    has_exact_answer: bool = Field(
-        description="If an exact answer was already generated for the question.")
-    has_ideal_answer: bool = Field(
-        description="If a human summary answer was already generated for the question.")
-    last_task: Task = Field(description="The last task that was run.")
-    last_task_successful: bool = Field(
-        description="Whether the last task was successful.")
+    history: Sequence[HistoryItem] = Field(
+        description="The history of tasks that were run from oldest to most recent.")
+    is_ready: bool = Field(
+        description="Whether the answer to the question is ready to be returned (i.e., by choosing 'none' as the next task).")
 
 
-class PredictNextTaskOutput(BaseModel):
+class NextTaskOutput(BaseModel):
     task: Task = Field(
-        description="The next task that should be run in order to answer the question. The task 'none' should only be chosen once all other tasks were run at least once.")
+        description="The next task that should be run in order to answer the question.")
 
 
 class PredictNextTask(Signature):
     """Chose the next task to run, in order to get the full answer. The next task should be chosen as to optimally answer the question given the data at hand. No specific order of the tasks is enforced. Tasks can be repeated."""
-    input: PredictNextTaskInput = InputField()
-    output: PredictNextTaskOutput = OutputField()
-
-
-_limiter = Limiter([
-    Rate(limit=1, interval=Duration.SECOND),
-    Rate(limit=500, interval=Duration.HOUR),
-], raise_when_fail=False)
-
-
-def _limiter_mapping(*_args, **_kwargs):
-    return "incremental", 1
-
-
-_T = TypeVar("_T")
-_P = ParamSpec("_P")
-
-
-def _limit(f: Callable[_P, _T]) -> Callable[_P, _T]:
-    return _limiter.as_decorator()(_limiter_mapping)(f)  # type: ignore
+    input: NextTaskInput = InputField()
+    output: NextTaskOutput = OutputField()
 
 
 class IncrementalAnswerModule(
@@ -98,26 +80,66 @@ class IncrementalAnswerModule(
         self._exact_answer_module = exact_answer_module
         self._ideal_answer_module = ideal_answer_module
 
-    @_limit
+    @rate_limit(Limiter([
+        Rate(limit=1, interval=Duration.SECOND),
+        Rate(limit=500, interval=Duration.HOUR),
+    ], raise_when_fail=False))
     def _next_task(
         self,
         builder: AnswerBuilder,
-        last_task: Task,
-        last_task_successful: bool,
+        history: Sequence[HistoryItem],
     ) -> Task:
-        input = PredictNextTaskInput(
+        input = NextTaskInput(
             question=builder.question.body,
             question_type=builder.question.type,
-            has_documents=builder.has_documents,
-            has_snippets=builder.has_snippets,
-            has_exact_answer=builder.has_exact_answer,
-            has_ideal_answer=builder.has_ideal_answer,
-            last_task=last_task,
-            last_task_successful=last_task_successful,
+            history=history,
+            is_ready=builder.is_ready,
         )
         prediction: Prediction = self._predict_next_task.forward(input=input)
-        output = cast(PredictNextTaskOutput, prediction.output)
+        output = cast(NextTaskOutput, prediction.output)
         return output.task
+
+    def _run_next_task(
+        self,
+        builder: AnswerBuilder,
+        history: Sequence[HistoryItem],
+    ) -> HistoryItem | None:
+        task = self._next_task(
+            builder=builder,
+            history=history,
+        )
+        if len(history) > 0 and task == history[-1].task:
+            warn("Cannot run the same task twice in a row.")
+            return HistoryItem(task=task, successful=False)
+        elif task == "retrieve documents":
+            print(
+                f"Making documents for question '{builder.question.body[:100]}'...")
+            builder.make_documents()
+            return HistoryItem(task=task, successful=True)
+        elif task == "retrieve snippets":
+            print(
+                f"Making snippets for question '{builder.question.body[:100]}'...")
+            builder.make_snippets()
+            return HistoryItem(task=task, successful=True)
+        elif task == "generate exact answer":
+            print(
+                f"Making exact for question '{builder.question.body[:100]}'...")
+            builder.make_exact_answer()
+            return HistoryItem(task=task, successful=True)
+        elif task == "generate summary answer":
+            print(
+                f"Making ideal for question '{builder.question.body[:100]}'...")
+            builder.make_ideal_answer()
+            return HistoryItem(task=task, successful=True)
+        elif task == "none":
+            if builder.is_ready:
+                print("Returning answer...")
+                return None
+            else:
+                warn("Attempted to return answer but answer was not ready yet.")
+                return HistoryItem(task=task, successful=False)
+        else:
+            raise RuntimeError(f"Unknown task: {task}")
 
     def forward(self, question: Question) -> Answer:
         builder = AnswerBuilder(
@@ -127,46 +149,15 @@ class IncrementalAnswerModule(
             exact_answer_module=self._exact_answer_module,
             ideal_answer_module=self._ideal_answer_module,
         )
-        next_task: Task = "none"
-        last_task: Task = "none"
-        last_task_successful: bool = True
-        while not builder.is_ready or next_task != "none":
-            next_task = self._next_task(
+        history: list[HistoryItem] = []
+        while not (builder.is_ready and
+                   len(history) > 0 and
+                   history[-1].task == "none"):
+            history_item = self._run_next_task(
                 builder=builder,
-                last_task=last_task,
-                last_task_successful=last_task_successful,
+                history=history,
             )
-            if next_task == last_task:
-                warn("Cannot run the same task twice in a row.")
-                last_task_successful = False
-            elif next_task == "retrieve documents":
-                print(
-                    f"Making documents for question '{builder.question.body[:100]}'...")
-                builder.make_documents()
-                last_task_successful = True
-            elif next_task == "retrieve snippets":
-                print(
-                    f"Making snippets for question '{builder.question.body[:100]}'...")
-                builder.make_snippets()
-                last_task_successful = True
-            elif next_task == "generate exact answer":
-                print(
-                    f"Making exact for question '{builder.question.body[:100]}'...")
-                builder.make_exact_answer()
-                last_task_successful = True
-            elif next_task == "generate summary answer":
-                print(
-                    f"Making ideal for question '{builder.question.body[:100]}'...")
-                builder.make_ideal_answer()
-                last_task_successful = True
-            elif next_task == "none":
-                if builder.is_ready:
-                    print("Returning answer...")
-                    return builder.answer
-                else:
-                    warn("Attempted to return answer but answer was not ready yet.")
-                    last_task_successful = False
-            else:
-                raise RuntimeWarning(f"Unknown task: {next_task}")
-            last_task = next_task
+            if history_item is None:
+                break
+            history.append(history_item)
         return builder.answer
