ac1f9dbfb4e9736ffb2b1c8f609abe540048a64b
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-04-08 19:44:31 +0200

    Add snippets folding

--- mibi/modules/documents/pyterrier.py
+++ mibi/modules/documents/pyterrier.py
@@ -1,12 +1,13 @@
 from pandas import DataFrame
 from pydantic_core import Url
+from pyterrier.transformer import Transformer
 
 from mibi.model import Documents
 from mibi.modules import DocumentsModule
 from mibi.utils.pyterrier import PyTerrierModule
 
 
-class PyTerrierDocumentsModule(DocumentsModule, PyTerrierModule[Documents]):
+class PyTerrierDocumentsModule(PyTerrierModule[Documents], DocumentsModule):
     def parse(self, res: DataFrame) -> Documents:
         if "docno" in res.columns:
             if any(res["docno"].isna()):
@@ -27,3 +28,22 @@ class PyTerrierDocumentsModule(DocumentsModule, PyTerrierModule[Documents]):
         else:
             raise ValueError(
                 f"Cannot parse documents from results with columns: {res.columns}")
+
+
+_SNIPPETS_COLS = {
+    "text",
+    "snippet_begin_section",
+    "snippet_offset_in_begin_section",
+    "snippet_end_section",
+    "snippet_offset_in_end_section",
+}
+
+
+class FoldSnippets(Transformer):
+    def transform(self, topics_or_res: DataFrame) -> DataFrame:
+        if _SNIPPETS_COLS.issubset(topics_or_res.columns):
+            topics_or_res = topics_or_res.groupby(
+                by=list(_SNIPPETS_COLS),
+                sort=False,
+            ).first().reset_index(drop=True)
+        return topics_or_res
