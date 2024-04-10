e7565ed4a4b22447b5185c1a881949bc63a74596
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-04-10 15:44:49 +0200

    Improve document retrieval

--- mibi/modules/documents/pipelines.py
+++ mibi/modules/documents/pipelines.py
@@ -1,6 +1,6 @@
 from typing import Any, Hashable
 from elasticsearch7 import Elasticsearch
-from elasticsearch7_dsl.query import Query, Match, Exists, Nested, Bool
+from elasticsearch7_dsl.query import Query, Match, Exists, Nested, Bool, Terms
 from pyterrier.transformer import Transformer
 from spacy import load as spacy_load
 from spacy.language import Language
@@ -10,6 +10,36 @@ from mibi.modules.documents.pubmed import Article
 from mibi.modules.documents.pyterrier import FoldSnippets
 from mibi.utils.elasticsearch_pyterrier import ElasticsearchTransformer
 from mibi.utils.pyterrier import ExportDocumentsTransformer
+
+_DISALLOWED_PUBLICATION_TYPES = [
+    "Letter",
+    "Comment",
+    "Editorial",
+    "News",
+    "Biography",
+    "Congress",
+    "Video-Audio Media",
+    "Interview",
+    "Overall",
+    "Retraction of Publication",
+    "Retracted Publication",
+    "Newspaper Article",
+    "Bibliography",
+    "Legal Case",
+    "Directory",
+    "Personal Narrative",
+    "Address",
+    "Randomized Controlled Trial, Veterinary",
+    "Autobiography",
+    "Dataset",
+    "Clinical Trial, Veterinary",
+    "Festschrift",
+    "Webcast",
+    "Observational Study, Veterinary",
+    "Dictionary",
+    "Periodical Index",
+    "Interactive Tutorial",
+]
 
 
 def build_documents_pipeline(
@@ -23,28 +53,47 @@ def build_documents_pipeline(
         language: Language = spacy_load("en_core_sci_sm")
         doc = language(query)
 
-        entities = doc.ents
-        entities_queries = [
-            Match(term=entity.text)
-            for entity in entities
-        ]
-
         query_stop_words_removed = " ".join(
-            token.text for token in doc if not token.is_stop)
+            token.text
+            for token in doc
+            if not token.is_stop
+        )
 
-        # TODO: Improve query.
-        es_query = (
-            Exists(field="abstract") &
-            (
-                (
-                    Match(title=query_stop_words_removed) |
-                    Match(abstract=query_stop_words_removed)
-                ) |
-                Nested(
-                    path="mesh_terms",
-                    query=Bool(should=entities_queries),
-                )
-            )
+        es_query = Bool(
+            filter=[
+                # Only consider articles with an abstract.
+                Exists(field="abstract"),
+                # Remove certain publication types.
+                ~Nested(
+                    path="publication_types",
+                    query=Terms(term=_DISALLOWED_PUBLICATION_TYPES),
+                ),
+            ],
+            must=[
+                Bool(
+                    must=[
+                        # Require to at least match the title or abstract.
+                        Bool(
+                            should=[
+                                Match(title=query_stop_words_removed),
+                                Match(abstract=query_stop_words_removed),
+                            ]
+                        )
+                    ],
+                    should=[
+                        # Prefer documents that also have matching MeSH terms.
+                        Nested(
+                            path="mesh_terms",
+                            query=Bool(
+                                should=[
+                                    Match(term=entity.text)
+                                    for entity in doc.ents
+                                ]
+                            ),
+                        ),
+                    ]
+                ),
+            ],
         )
         return es_query
 
@@ -52,15 +101,56 @@ def build_documents_pipeline(
         return {
             "title": article.title,
             "abstract": article.abstract,
-            # TODO: Maybe we want the concatenated title and abstract here?
+            # TODO: Do we want the concatenated title and abstract or just the abstract here?
             "text": article.abstract,
+            # "text": f"{article.title} {article.abstract}",
             "url": article.pubmed_url,
             "doi": article.doi,
             "doi_url": article.doi_url,
-            # TODO: Add more article fields.
+            "pmc_id": article.pmc_id,
+            "pmc_url": article.pmc_url,
+            "nlm_id": article.nlm_id,
+            "author_full_names": [
+                f"{author.forename} {author.lastname}"
+                for author in article.authors
+                if author.forename is not None and author.lastname is not None
+            ],
+            "author_orcids": [
+                author.orcid
+                for author in article.authors
+                if author.orcid is not None
+            ],
+            "mesh_terms": [
+                term.term
+                for term in article.mesh_terms
+            ],
+            "publication_types": [
+                term.term
+                for term in article.publication_types
+            ],
+            "chemicals": [
+                term.term
+                for term in article.chemicals
+            ],
+            "keywords": list(article.keywords),
+            "all_terms": [
+                term.term
+                for terms in (
+                    article.mesh_terms,
+                    article.publication_types,
+                    article.chemicals,
+                )
+                for term in terms
+            ] + list(article.keywords),
+            "publication_date": article.publication_date,
+            "journal": article.journal,
+            "journal_abbreviation": article.journal_abbreviation,
+            "issn": article.issn,
+            "country": article.country,
+            "languages": article.languages,
         }
 
-    pipeline = pipeline >> ElasticsearchTransformer(
+    pipeline = (pipeline >> ElasticsearchTransformer(
         document_type=Article,
         client=elasticsearch,
         query_builder=build_query,
@@ -68,7 +158,7 @@ def build_documents_pipeline(
         num_results=10,
         index=index,
         verbose=True,
-    )
+    ))
 
     # TODO: Re-rank documents.
 
@@ -76,8 +166,7 @@ def build_documents_pipeline(
     pipeline = pipeline % 10  # type: ignore
 
     # FIXME: Export documents temporarily, to manually import them to the answer generation stage.
-    pipeline = pipeline >> ExportDocumentsTransformer(path=PROJECT_DIR / "data" / "documents")
+    pipeline = pipeline >> ExportDocumentsTransformer(
+        path=PROJECT_DIR / "data" / "documents")
 
     return pipeline
-
-
