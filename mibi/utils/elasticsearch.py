0161a5939616067ae7379347b42fec2e003acd98
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-04-15 11:39:20 +0200

    Add ES helper

--- mibi/utils/elasticsearch.py
+++ mibi/utils/elasticsearch.py
@@ -56,3 +56,30 @@ class ElasticsearchIndexer(Generic[T]):
     def index_all(self, articles: Iterable[T]) -> None:
         for _ in self.iter_index(articles):
             pass
+
+
+def elasticsearch_connection(
+    elasticsearch_url: str | None,
+    elasticsearch_username: str | None,
+    elasticsearch_password: str | None,
+) -> Elasticsearch | None:
+    if elasticsearch_url is None:
+        return None
+
+    elasticsearch_auth: tuple[str, str] | None
+    if elasticsearch_username is not None and elasticsearch_password is None:
+        raise ValueError("Must provide both username and password or neither.")
+    elif elasticsearch_password is not None and elasticsearch_username is None:
+        raise ValueError("Must provide both password and username or neither.")
+    elif elasticsearch_username is not None and elasticsearch_password is not None:
+        elasticsearch_auth = (elasticsearch_username, elasticsearch_password)
+    else:
+        elasticsearch_auth = None
+
+    return Elasticsearch(
+        hosts=elasticsearch_url,
+        http_auth=elasticsearch_auth,
+        request_timeout=60,
+        read_timeout=60,
+        max_retries=10,
+    )
