b4e19df4f8197db95201c51942d7c477f01aac5c
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-04-15 11:39:59 +0200

    Add pipeline setup helper

---
+++ mibi/modules/build.py
@@ -0,0 +1,140 @@
+from typing import Literal
+
+from pyterrier import started, init
+
+from mibi.modules import AnswerModule, DocumentsModule, ExactAnswerModule, IdealAnswerModule, SnippetsModule
+from mibi.modules.documents.pipelines import build_documents_pipeline
+from mibi.modules.documents.pyterrier import PyTerrierDocumentsModule
+from mibi.modules.exact_answer.llm import LlmExactAnswerModule
+from mibi.modules.ideal_answer.llm import LlmIdealAnswerModule
+from mibi.modules.incremental import IncrementalAnswerModule
+from mibi.modules.independent import IndependentAnswerModule
+from mibi.modules.mock import MockDocumentsModule, MockExactAnswerModule, MockIdealAnswerModule, MockSnippetsModule
+from mibi.modules.snippets.pipelines import build_snippets_pipeline
+from mibi.modules.snippets.pyterrier import PyTerrierSnippetsModule
+from mibi.modules.standard import StandardAnswerModule
+from mibi.utils.elasticsearch import elasticsearch_connection
+from mibi.utils.language_models import init_language_model_clients
+
+
+def build_answer_module(
+    documents_module_type: Literal[
+        "mock",
+        "pyterrier",
+    ],
+    snippets_module_type: Literal[
+        "mock",
+        "pyterrier",
+    ],
+    exact_answer_module_type: Literal[
+        "mock",
+    ],
+    ideal_answer_module_type: Literal[
+        "mock",
+    ],
+    answer_module_type: Literal[
+        "standard",
+        "incremental",
+        "independent",
+    ],
+    language_model_name: Literal[
+        "gpt-3.5-turbo",
+        "gpt-3.5-turbo-0125",
+        "text-davinci-003",
+        "Mixtral-8x7B-Instruct-v0.1",
+        "Mistral-7B-Instruct-v0.2",
+    ],
+    elasticsearch_url: str | None,
+    elasticsearch_username: str | None,
+    elasticsearch_password: str | None,
+    elasticsearch_index: str | None,
+) -> AnswerModule:
+
+    # Init language models.
+    init_language_model_clients(language_model_name)
+
+    # Init Elasticsearch.
+    elasticsearch = elasticsearch_connection(
+        elasticsearch_url,
+        elasticsearch_username,
+        elasticsearch_password,
+    )
+
+    # Create documents module.
+    documents_module: DocumentsModule
+    if documents_module_type == "mock":
+        documents_module = MockDocumentsModule()
+    elif documents_module_type == "pyterrier":
+        if not started():
+            init()
+        if elasticsearch is None or elasticsearch_index is None:
+            raise ValueError("Must provide Elasticsearch URL and index.")
+        pipeline = build_documents_pipeline(
+            elasticsearch=elasticsearch,
+            index=elasticsearch_index,
+        )
+        documents_module = PyTerrierDocumentsModule(pipeline)
+    else:
+        raise ValueError("Unknown documents module type.")
+
+    # Create snippets module.
+    snippets_module: SnippetsModule
+    if snippets_module_type == "mock":
+        snippets_module = MockSnippetsModule()
+    elif snippets_module_type == "pyterrier":
+        if not started():
+            init()
+        if elasticsearch is None or elasticsearch_index is None:
+            raise ValueError("Must provide Elasticsearch URL and index.")
+        pipeline = build_snippets_pipeline(
+            elasticsearch=elasticsearch,
+            index=elasticsearch_index,
+        )
+        snippets_module = PyTerrierSnippetsModule(pipeline)
+    else:
+        raise ValueError("Unknown snippets module type.")
+
+    # Create exact answer module.
+    exact_answer_module: ExactAnswerModule
+    if exact_answer_module_type == "mock":
+        exact_answer_module = MockExactAnswerModule()
+    elif exact_answer_module_type == "llm":
+        exact_answer_module = LlmExactAnswerModule()
+    else:
+        raise ValueError("Unknown exact answer module type.")
+
+    # Create ideal answer module.
+    ideal_answer_module: IdealAnswerModule
+    if ideal_answer_module_type == "mock":
+        ideal_answer_module = MockIdealAnswerModule()
+    elif exact_answer_module_type == "llm":
+        ideal_answer_module = LlmIdealAnswerModule()
+    else:
+        raise ValueError("Unknown ideal answer module type.")
+
+    # Assemble full answer module.
+    answer_module: AnswerModule
+    if answer_module_type == "standard":
+        answer_module = StandardAnswerModule(
+            documents_module=documents_module,
+            snippets_module=snippets_module,
+            exact_answer_module=exact_answer_module,
+            ideal_answer_module=ideal_answer_module,
+        )
+    elif answer_module_type == "incremental":
+        answer_module = IncrementalAnswerModule(
+            documents_module=documents_module,
+            snippets_module=snippets_module,
+            exact_answer_module=exact_answer_module,
+            ideal_answer_module=ideal_answer_module,
+        )
+    elif answer_module_type == "independent":
+        answer_module = IndependentAnswerModule(
+            documents_module=documents_module,
+            snippets_module=snippets_module,
+            exact_answer_module=exact_answer_module,
+            ideal_answer_module=ideal_answer_module,
+        )
+    else:
+        raise ValueError("Unknown documents module type.")
+    return answer_module
