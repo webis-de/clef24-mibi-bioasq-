92a3453e71271c7fbc5b2b8417b2f584f414f2ee
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-04-24 15:35:27 +0200

    Improve documents pipeline - allow for mixing documents and snippets - answer-based query expansion

--- mibi/modules/documents/pipelines.py
+++ mibi/modules/documents/pipelines.py
@@ -12,14 +12,9 @@ from spacy.language import Language
 
 from mibi import PROJECT_DIR
 from mibi.modules.documents.pubmed import Article
-from mibi.modules.snippets.pyterrier import SNIPPETS_COLS
 from mibi.utils.elasticsearch import elasticsearch_connection
 from mibi.utils.elasticsearch_pyterrier import ElasticsearchTransformer
-from mibi.utils.pyterrier import ConditionalTransformer, ExportDocumentsTransformer
-
-
-def _has_snippet_columns(topics_or_res: DataFrame) -> bool:
-    return SNIPPETS_COLS.issubset(topics_or_res.columns)
+from mibi.utils.pyterrier import ExportDocumentsTransformer, MaybeDePassager
 
 
 _DISALLOWED_PUBLICATION_TYPES = [
@@ -53,9 +48,7 @@ _DISALLOWED_PUBLICATION_TYPES = [
 ]
 
 
-
-
-def _build_query(row: dict[Hashable, Any]) -> Query:
+def build_query(row: dict[Hashable, Any]) -> Query:
     query = str(row["query"])
 
     # TODO: Rank based on the query type.
@@ -111,57 +104,12 @@ def _build_query(row: dict[Hashable, Any]) -> Query:
     return es_query
 
 
-def _build_result(article: Article) -> dict[Hashable, Any]:
+def build_result(article: Article) -> dict[Hashable, Any]:
     return {
         "title": article.title,
         "abstract": article.abstract,
-        # TODO: Do we want the concatenated title and abstract or just the abstract here?
-        "text": article.abstract,
-        # "text": f"{article.title} {article.abstract}",
+        "text": f"{article.title} {article.abstract}",
         "url": article.pubmed_url,
-        "doi": article.doi,
-        "doi_url": article.doi_url,
-        "pmc_id": article.pmc_id,
-        "pmc_url": article.pmc_url,
-        "nlm_id": article.nlm_id,
-        "author_full_names": [
-            f"{author.forename} {author.lastname}"
-            for author in article.authors
-            if author.forename is not None and author.lastname is not None
-        ],
-        "author_orcids": [
-            author.orcid
-            for author in article.authors
-            if author.orcid is not None
-        ],
-        "mesh_terms": [
-            term.term
-            for term in article.mesh_terms
-        ],
-        "publication_types": [
-            term.term
-            for term in article.publication_types
-        ],
-        "chemicals": [
-            term.term
-            for term in article.chemicals
-        ],
-        "keywords": list(article.keywords),
-        "all_terms": [
-            term.term
-            for terms in (
-                article.mesh_terms,
-                article.publication_types,
-                article.chemicals,
-            )
-            for term in terms
-        ] + list(article.keywords),
-        "publication_date": article.publication_date,
-        "journal": article.journal,
-        "journal_abbreviation": article.journal_abbreviation,
-        "issn": article.issn,
-        "country": article.country,
-        "languages": article.languages,
     }
 
 
@@ -172,17 +120,16 @@ def _expand_query(row: Series) -> str:
         exact_answer = str(row["exact_answer"])
         exact_answer = exact_answer.capitalize().removesuffix(".")
         query = f"{query} {exact_answer}."
-        print(query)
 
     if "ideal_answer" in row.keys():
         ideal_answer = str(row["ideal_answer"])
         ideal_answer = ideal_answer.capitalize().removesuffix(".")
         query = f"{query} {ideal_answer}."
-        print(query)
 
     return query
 
-expand_query = query(_expand_query)
+
+expand_query = query(_expand_query, verbose=True)
 
 
 @dataclass(frozen=True)
@@ -194,33 +141,31 @@ class DocumentsPipeline(Transformer):
 
     @cached_property
     def _pipeline(self) -> Transformer:
-        # If snippets are given, de-passage them.
-        de_passager = MaxPassage()
-        pipeline = ConditionalTransformer(
-            condition=_has_snippet_columns,
-            transformer_true=de_passager,
-            transformer_false=Transformer.identity(),
-        )
+        pipeline = Transformer.identity()
 
         # Expand the query with previous answers.
         pipeline = pipeline >> expand_query
 
+        # Snippets need to be de-passaged, but oterwise skip de-passaging.
+        de_passager = MaxPassage()
+        de_passager = MaybeDePassager(de_passager)
+        pipeline = pipeline >> de_passager
+
         # Retrieve or re-rank documents with Elasticsearch (BM25).
-        pipeline = pipeline >> ElasticsearchTransformer(
+        elasticsearch = ElasticsearchTransformer(
             document_type=Article,
             client=elasticsearch_connection(
                 elasticsearch_url=self.elasticsearch_url,
                 elasticsearch_username=self.elasticsearch_username,
                 elasticsearch_password=self.elasticsearch_password,
             ),
-            query_builder=_build_query,
-            result_builder=_build_result,
+            query_builder=build_query,
+            result_builder=build_result,
             num_results=10,
             index=self.elasticsearch_index,
             verbose=True,
         )
-
-        # TODO: Re-rank documents?
+        pipeline = pipeline >> elasticsearch
 
         # Cut off at 10 documents as per BioASQ requirements.
         pipeline = pipeline % 10  # type: ignore
