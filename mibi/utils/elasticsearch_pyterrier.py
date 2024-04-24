e202ae012a332587d185f87571ada377ffedc452
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-04-24 15:26:28 +0200

    Improve ES-PyTerrier compat

--- mibi/utils/elasticsearch_pyterrier.py
+++ mibi/utils/elasticsearch_pyterrier.py
@@ -121,26 +121,27 @@ class ElasticsearchRerank(Generic[T], Transformer):
     verbose: bool = False
 
     def _transform_query(self, res: DataFrame) -> DataFrame:
+        docnos = res["docno"]
+        if docnos.isna().any():
+            raise RuntimeError("Cannot re-rank documents with missing docno.")
         search: Search = self.document_type.search(
             using=self.client, index=self.index)
         search = search.query(self.query_builder(res.iloc[0].to_dict()))
-        search = search.filter(Terms(_id=[
-            row["docno"]
-            for _, row in res.iterrows()
-        ]))
+        search = search.filter(Terms(_id=list(set(docnos))))
         search = search.extra(size=len(res))
         search = search.extra(_source=False)
 
         response = search.execute()
         hits: Iterable[Hit] = response.hits.hits  # type: ignore
-        scores = DataFrame([
-            {
-                "docno": hit._id,
-                "score": hit._score,
-            }
+        scores_list: list[tuple[str, float]] = [  # type: ignore
+            (hit._id, hit._score)
             for hit in hits
-        ])
-
+        ]
+        scores = DataFrame({
+            "docno": [docno for docno, _ in scores_list],
+            "score": [score for _, score in scores_list],
+        })
+        res.drop(columns="score", inplace=True)
         res = res.merge(scores, how="left", on="docno")
         if res["score"].isna().sum() > 0:
             not_reranked = res[res["score"].isna()]["docno"]
@@ -210,8 +211,6 @@ class ElasticsearchGet(Generic[T], Transformer):
         }
 
     def _transform_query(self, res: DataFrame) -> DataFrame:
-        if not isinstance(res, DataFrame):
-            raise RuntimeError("Can only transform data frames.")
         if "docno" not in res.columns:
             raise RuntimeError("Needs docno column.")
         if len(res) == 0:
@@ -232,8 +231,6 @@ class ElasticsearchGet(Generic[T], Transformer):
         ])
 
     def transform(self, topics_or_res: DataFrame) -> DataFrame:
-        if not isinstance(topics_or_res, DataFrame):
-            raise RuntimeError("Can only transform data frames.")
         if "docno" not in topics_or_res.columns:
             raise RuntimeError("Needs docno column.")
         if len(topics_or_res) == 0:
@@ -259,6 +256,50 @@ class ElasticsearchGet(Generic[T], Transformer):
 
         topics_or_res.reset_index(drop=True, inplace=True)
         return topics_or_res
+
+
+@dataclass(frozen=True)
+class ElasticsearchRerankGet(Generic[T], Transformer):
+    """
+    Re-ranks documents and get document fields from an Elasticsearch index.
+
+    :param document_type: Elasticsearch document type. Must extend `Document`.
+    :param client: Elasticsearch client to execute searches.
+    :param query_builder: A function that builds an Elasticsearch query from the data frame row.
+    :param result_builder: A function that extracts a dict from the document returned by Elasticsearch.
+    :param num_results: Number of results to be retrieved. Defaults to 10 results.
+    :param index: The Elasticsearch index name to retrieve documents from. Defaults to the index specified in the document type.
+    :param verbose: Whether to show a progress bar when retrieving/re-ranking results. Defaults to `False`.
+    """
+    document_type: Type[T]
+    client: Elasticsearch
+    query_builder: Callable[[dict[Hashable, Any]], Query] = field(repr=False)
+    result_builder: Callable[[T], dict[Hashable, Any]] = field(repr=False)
+    index: str | None = None
+    verbose: bool = False
+
+    @cached_property
+    def _rerank(self) -> ElasticsearchRerank:
+        return ElasticsearchRerank(
+            document_type=self.document_type,
+            client=self.client,
+            query_builder=self.query_builder,
+            index=self.index,
+            verbose=self.verbose,
+        )
+
+    @cached_property
+    def _get(self) -> ElasticsearchGet:
+        return ElasticsearchGet(
+            document_type=self.document_type,
+            client=self.client,
+            result_builder=self.result_builder,
+            index=self.index,
+            verbose=self.verbose,
+        )
+
+    def transform(self, topics_or_res: DataFrame) -> DataFrame:
+        return (self._rerank >> self._get).transform(topics_or_res)
 
 
 @dataclass(frozen=True)
@@ -295,30 +336,19 @@ class ElasticsearchTransformer(Generic[T], Transformer):
         )
 
     @cached_property
-    def _rerank(self) -> ElasticsearchRerank:
-        return ElasticsearchRerank(
+    def _rerank_get(self) -> ElasticsearchRerankGet:
+        return ElasticsearchRerankGet(
             document_type=self.document_type,
             client=self.client,
             query_builder=self.query_builder,
-            index=self.index,
-            verbose=self.verbose,
-        )
-
-    @cached_property
-    def _get(self) -> ElasticsearchGet:
-        return ElasticsearchGet(
-            document_type=self.document_type,
-            client=self.client,
             result_builder=self.result_builder,
             index=self.index,
             verbose=self.verbose,
         )
 
     def transform(self, topics_or_res: DataFrame) -> DataFrame:
-        if not isinstance(topics_or_res, DataFrame):
-            raise RuntimeError("Can only transform data frames.")
         if "docno" in topics_or_res.columns:
-            return ((self._rerank >> self._get) ^ self._retrieve).transform(topics_or_res)
+            return (self._rerank_get ^ self._retrieve).transform(topics_or_res)
         if {"qid", "query"}.issubset(topics_or_res.columns):
             return self._retrieve.transform(topics_or_res)
         raise RuntimeError("Needs qid and query columns or docno column.")
