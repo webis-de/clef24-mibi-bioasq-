d13450da5e748b3e2737462071fc6f8d6c2a3307
Jan Heinrich Reimer <heinrich.reimer@uni-jena.de>
2024-05-12 19:43:58 +0200

    Fix tests

--- mibi/test_json_schema.py
+++ mibi/test_json_schema.py
@@ -1,110 +1,115 @@
+from pydantic import TypeAdapter
 from mibi.model import Answer, AnsweredQuestion, AnsweredQuestionData, PartialAnswer, PartiallyAnsweredQuestion, PartiallyAnsweredQuestionData, Question, QuestionData, Snippet
 
 
 def test_question_json_validation_schema() -> None:
-    schema = Question.model_json_schema(mode="validation")
+    schema = TypeAdapter(Question).json_schema(mode="validation")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_question_json_serialization_schema() -> None:
-    schema = Question.model_json_schema(mode="serialization")
+    schema = TypeAdapter(Question).json_schema(mode="serialization")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_question_data_json_validation_schema() -> None:
-    schema = QuestionData.model_json_schema(mode="validation")
+    schema = TypeAdapter(QuestionData).json_schema(mode="validation")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_question_data_json_serialization_schema() -> None:
-    schema = QuestionData.model_json_schema(mode="serialization")
+    schema = TypeAdapter(QuestionData).json_schema(mode="serialization")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_snippet_json_validation_schema() -> None:
-    schema = Snippet.model_json_schema(mode="validation")
+    schema = TypeAdapter(Snippet).json_schema(mode="validation")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_snippet_json_serialization_schema() -> None:
-    schema = Snippet.model_json_schema(mode="serialization")
+    schema = TypeAdapter(Snippet).json_schema(mode="serialization")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_answer_json_validation_schema() -> None:
-    schema = Answer.model_json_schema(mode="validation")
+    schema = TypeAdapter(Answer).json_schema(mode="validation")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_answer_json_serialization_schema() -> None:
-    schema = Answer.model_json_schema(mode="serialization")
+    schema = TypeAdapter(Answer).json_schema(mode="serialization")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_partial_answer_json_validation_schema() -> None:
-    schema = PartialAnswer.model_json_schema(mode="validation")
+    schema = TypeAdapter(PartialAnswer).json_schema(mode="validation")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_partial_answer_json_serialization_schema() -> None:
-    schema = PartialAnswer.model_json_schema(mode="serialization")
+    schema = TypeAdapter(PartialAnswer).json_schema(mode="serialization")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_answered_question_json_validation_schema() -> None:
-    schema = AnsweredQuestion.model_json_schema(mode="validation")
+    schema = TypeAdapter(AnsweredQuestion).json_schema(mode="validation")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_answered_question_json_serialization_schema() -> None:
-    schema = AnsweredQuestion.model_json_schema(mode="serialization")
+    schema = TypeAdapter(AnsweredQuestion).json_schema(mode="serialization")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_answered_question_data_json_validation_schema() -> None:
-    schema = AnsweredQuestionData.model_json_schema(mode="validation")
+    schema = TypeAdapter(AnsweredQuestionData).json_schema(mode="validation")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_answered_question_data_json_serialization_schema() -> None:
-    schema = AnsweredQuestionData.model_json_schema(mode="serialization")
+    schema = TypeAdapter(AnsweredQuestionData).json_schema(
+        mode="serialization")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_partially_answered_question_json_validation_schema() -> None:
-    schema = PartiallyAnsweredQuestion.model_json_schema(mode="validation")
+    schema = TypeAdapter(PartiallyAnsweredQuestion).json_schema(
+        mode="validation")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_partially_answered_question_json_serialization_schema() -> None:
-    schema = PartiallyAnsweredQuestion.model_json_schema(mode="serialization")
+    schema = TypeAdapter(PartiallyAnsweredQuestion).json_schema(
+        mode="serialization")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_partially_answered_question_data_json_validation_schema() -> None:
-    schema = PartiallyAnsweredQuestionData.model_json_schema(mode="validation")
+    schema = TypeAdapter(PartiallyAnsweredQuestionData).json_schema(
+        mode="validation")
     assert "type" in schema
     assert schema["type"] == "object"
 
 
 def test_partially_answered_question_data_json_serialization_schema() -> None:
-    schema = PartiallyAnsweredQuestionData.model_json_schema(
+    schema = TypeAdapter(PartiallyAnsweredQuestionData).json_schema(
         mode="serialization")
     assert "type" in schema
     assert schema["type"] == "object"
